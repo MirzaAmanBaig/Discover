5ab5950760a0c7e578b58f16f3b2d4454c66000b
MirzaAmanBaig <88486348+MirzaAmanBaig@users.noreply.github.com>
2021-08-07 01:12:12 +0530

    commit

--- account/views.py
+++ account/views.py
@@ -5,10 +5,12 @@ from django.contrib.auth.models import User,auth
 from .models import Destination
 from .models import Contact
 from django.views.decorators.cache import cache_control
+from django.contrib.auth.decorators import login_required
 
 
 
 # Create your views here.
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def register(request):
     if request.method=='POST':
         first_name=request.POST['first_name']
@@ -17,8 +19,6 @@ def register(request):
         password1=request.POST['password1']
         password2=request.POST['password2']
         email=request.POST['email']
-
-     
         if password1==password2:
             if User.objects.filter(username=username).exists():
                 messages.info(request,"username already exists")
@@ -44,25 +44,23 @@ def login(request):
     if request.method=='POST':
         username=request.POST['username']
         password=request.POST['password']
-
         user=auth.authenticate(username=username,password=password)
-
         if user is not None:
             auth.login(request,user)
             return redirect("/")
         else:
             messages.warning(request,"invalid credentials")
-            return redirect("login")
-           
+            return render(request,'login.html')
     else:
         return render(request,'login.html')
 
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@login_required(login_url='login')
 def logout(request):
     auth.logout(request)
-    return redirect("/")
+    return redirect("login")
 
-
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def contact(request):
     if request.method=='POST':
         firstname=request.POST['firstname']
@@ -73,13 +71,17 @@ def contact(request):
         contact.save()
     return render(request,"contact.html")
 
-
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def about(request):
     return render(request,"about.html")
 
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@login_required(login_url='login')
 def shops(request):
     dests=Destination.objects.all()
     return render(request,"shops.html",{"dests":dests})
 
       
+
+
+    
